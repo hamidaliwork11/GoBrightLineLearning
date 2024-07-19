a61da5c4659cd3efcf9d37b791d850fb580b9efd
Almab Hira <almab.hira@fiveriverstech.com>
2024-07-19 11:21:34 +0500

    hello

---
+++ cypress/PageLocators/parkingPass.js
@@ -0,0 +1,22 @@
+
+
+const parkingPass = {
+
+
+    getMainDropdown : '[class="blte-nav-item-header__label"]',
+    getParkingpassClick : '[href="https://www.gobrightline.com/parking-passes"]',
+    getGarageDropdown : '[role="combobox" ]>[name="ppStationName"]',
+    getvalueFromDropdown : '[class="Popover-body"]>[id=":r4:-list"]',
+    getstateClick : '[role="combobox"]>[name="ppLicenseState"]',
+    getStatefromDropdown : '[class="blte-desktop-select__listItems"]',
+    getPlatefieldClick : '[id="input-:r6:"]',
+    getValuefromPlatefield : '[id="input-:r6:"]',
+    getCheckboxClick : '[id=":r7:"]',
+    getAddtocartClick : '[class="blte-monthly-parking-passes-form__btn-wrapper"]>[type="button"]',
+    getTotalCost : '[class="blte-text blte-passes-cost-summary-item__price blte-passes-cost-summary-item__price--isTotal"]',
+    getEditButtonClick : 'button[class="blte-btn blte-btn--color-blue blte-btn--size-medium blte-btn--width-normal blte-btn--variant-secondary"]'
+}
+
+
+export {parkingPass}
+
